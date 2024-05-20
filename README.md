2ada58dfedbdbce4ef43c00d59a415b5848f17da
SEduardo-36524 <166569549+SEduardo-36524@users.noreply.github.com>
2024-05-20 17:00:28 -0300

    Debug=False, add_README

---
+++ README.md
@@ -0,0 +1,2 @@
+Meu primeiro projeto de site utilizando a linguagem python como back end.
+Este site é uma réplica básica do Pinterest.
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from fakepinterest import app
 # Roda o site apenas se o arquivo main for executado, não permite rodar por meio de importação
 if __name__ == "__main__":
     # debug=True: Toda alteração no código é transmitida ao site sem a necessidade de reiniciar o programa
-    app.run(debug=True)
+    app.run(debug=False)
