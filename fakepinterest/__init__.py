6f2f3c114e8bc43a219909693bced3098de20122
SEduardo-36524 <166569549+SEduardo-36524@users.noreply.github.com>
2024-05-21 17:56:20 -0300

    Alteração para banco postgreesql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,11 +3,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 # Cria o aplicativo
 app = Flask(__name__)
 # Local do banco de dados
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 # Chave de segurança do aplicativo
 app.config["SECRET_KEY"] = "258c8aa7f0d8dcad40048a070d26c8bdfca4b5a1398f73da67dcc72ee3876351"
 # Caminho de upload das fotos
